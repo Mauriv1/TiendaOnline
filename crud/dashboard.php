b7e0e5434f0e36d1520558ce2f0fffaafaed6282
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-08-12 20:56:40 -0500

    commit-ultimo

--- crud/dashboard.php
+++ crud/dashboard.php
@@ -34,12 +34,11 @@
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
-                        <a class="nav-link" href="http://localhost/TiendaOnline/crud/dashboard.php">Lista de Productos <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="http://localhost/TiendaOnline/crud/dashboard.php">Lista de Productos<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="http://localhost/TiendaOnline/crud/listaProveedor.php">Lista de Provedores</a>
                     </li>
-                    
                 </ul>
             </div>
         </nav>
